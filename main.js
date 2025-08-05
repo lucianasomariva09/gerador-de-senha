a4b36cc4919dce898a85b3b6acd539942283b0a6
lucianasomariva09 <159184331+lucianasomariva09@users.noreply.github.com>
2025-08-05 11:20:47 -0300

    05

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 5;
+let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
-const botoes = document.querySelectorAll('.parametro-senha__botao')
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!#*?%@';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -27,17 +33,34 @@ function aumentaTamanho(){
     geraSenha();
 } 
 
-const campoSenha = document.querySelector('#campo-senha');
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-geraSenha();
 
+for(i = 0; i < checkbox.length; i++){
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha()
 function geraSenha(){
-    let senha = ''
-    for(let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = SENHA - letrasMaiusculas(numeroAleatorio);
-
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
     }
-    campoSenha.volume = senha;
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+
+    let senha = '';
+    for(let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto(numeroAleatorio);
+        campoSenha.volue = senha;
+        classificaSenha();
+    }
+    
 }
