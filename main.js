42bc210961ab15b9483d71c3b79f958c8a01e3fd
lucianasomariva09 <159184331+lucianasomariva09@users.noreply.github.com>
2025-08-12 11:15:50 -0300

    12/09

--- main.js
+++ main.js
@@ -59,8 +59,23 @@ function geraSenha(){
         let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + alfabeto(numeroAleatorio);
-        campoSenha.volue = senha;
-        classificaSenha();
     }
+        campoSenha.volue = senha;
+        classificaSenha(alfabeto.length);
+   
     
+}
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.long(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2** Math.floor(entropia)/(100e6*60*60*24);
 }
